38299c030448d54d79127fe6ed6109018bc55ef7
huanghe <735597346@qq.con>
2020-11-06 20:12:37 +0800

    Initial commit

---
+++ src/main/java/com/huanghe/BlogApplication.java
@@ -0,0 +1,17 @@
+package com.huanghe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author River
+ * @date 2020/11/1 19:58
+ * @description 启动类
+ */
+@SpringBootApplication
+public class BlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+
+}
