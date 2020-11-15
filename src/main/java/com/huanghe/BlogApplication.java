34b7ec6c17e4db6116001b950a27fd900d588016
huanghe <735597346@qq.con>
2020-11-15 19:11:19 +0800

    集成swagger

--- src/main/java/com/huanghe/BlogApplication.java
+++ src/main/java/com/huanghe/BlogApplication.java
@@ -4,18 +4,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author River
  * @date 2020/11/1 19:58
  * @description 启动类
  */
-@SpringBootApplication
+
 @Slf4j
+@EnableSwagger2
 @MapperScan(basePackages = {"com.huanghe.dao"})
+@SpringBootApplication
 public class BlogApplication {
     public static void main(String[] args) {
-        log.info("开始启动率");
         SpringApplication.run(BlogApplication.class, args);
     }
 
